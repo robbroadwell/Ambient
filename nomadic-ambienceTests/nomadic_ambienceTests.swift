bf547bba10c5c1df33b9b088273580aaa8ea498d
Rob Broadwell <hello@robbroadwell.com>
2018-01-08 22:06:48 -0700

    MVP Design + Initial

---
+++ nomadic-ambienceTests/nomadic_ambienceTests.swift
@@ -0,0 +1,36 @@
+//
+//  nomadic_ambienceTests.swift
+//  nomadic-ambienceTests
+//
+//  Created by Rob Broadwell on 1/8/18.
+//  Copyright © 2018 Rob Broadwell. All rights reserved.
+//
+
+import XCTest
+@testable import nomadic_ambience
+
+class nomadic_ambienceTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
